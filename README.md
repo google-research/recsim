a678a32963f3b1eb8c5eadac9f51db27753ec8f0
ChihWei Hsu <cwhsu@google.com>
2019-09-25 16:31:55 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
+# RecSim: A Configurable Recommender Systems Simulation Platform
 
+RecSim is a configurable platform for authoring simulation environments for
+recommender systems (RSs) that naturally supports **sequential interaction**
+with users. RecSim allows the creation of new environments that reflect
+particular aspects of user behavior and item structure at a level of abstraction
+well-suited to pushing the limits of current reinforcement learning (RL) and RS
+techniques in sequential interactive recommendation problems. Environments can
+be easily configured that vary assumptions about: user preferences and item
+familiarity; user latent state and its dynamics; and choice models and other
+user response behavior. We outline how RecSim offers value to RL and RS
+researchers and practitioners, and how it can serve as a vehicle for
+academic-industrial collaboration.
+
+<a id='Disclaimer'></a>
+## Disclaimer
+
+This is not an officially supported Google product.
+
+## Installation and Sample Usage
+
+It is recommended to install RecSim using (https://pypi.org/project/recsim/):
+
+```shell
+pip install recsim
+```
+
+Here are some sample commands you could use for testing the installation:
+
+```
+git clone https://github.com/google-research/recsim
+cd recsim
+python main.py --logtostderr \
+  --base_dir="/tmp/recsim/interest_exploration_full_slate_q" \
+  --agent_name=full_slate_q \
+  --environment_name=interest_exploration \
+  --episode_log_file='episode_logs.tfrecord' \
+  --gin_bindings=simulator.runner_lib.Runner.max_steps_per_episode=100 \
+  --gin_bindings=simulator.runner_lib.TrainRunner.num_iterations=10 \
+  --gin_bindings=simulator.runner_lib.TrainRunner.max_training_steps=100 \
+  --gin_bindings=simulator.runner_lib.EvalRunner.max_eval_episodes=5 \
+```
+
+You could then start a tensorboard and view the output
+
+```
+tensorboard --logdir=/tmp/recsim/interest_exploration_full_slate_q/ --port=2222
+```
+
+You could also find the simulated logs in /tmp/recsim/episode_logs.tfrecord
+
+## Tutorials
+
+To get started, please check out our Colab tutorials. In [RecSim:
+Overview](colab/RecSim_Overview.ipynb), we give a brief overview about
+RecSim. We then talk about each configurable component:
+[**environment**](colab/RecSim_Developing_an_Environment.ipynb) and
+[**recommender agent**](colab/RecSim_Developing_an_Agent.ipynb).
+
+## Documentation
+
+API documentation can be found in [SITEMAP](docs/SITEMAP.md).
+
+Please refer to the [white paper](http://arxiv.org/abs/1909.04847) for the
+high-level design.
---
+++ setup.py
@@ -0,0 +1,87 @@
+# coding=utf-8
+# Copyright 2019 The RecSim Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Setup script for RecSim.
+
+This script will install RecSim as a Python module.
+
+See: https://github.com/google-research/recsim
+
+"""
+
+from os import path
+from setuptools import find_packages
+from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+
+install_requires = [
+    'absl-py',
+    'atari-py',  # Used by Dopamine
+    'dopamine-rl',
+    'gin-config',
+    'gym',
+    'numpy',
+    'tensorflow',
+]
+
+recsim_description = (
+    'RecSim: A Configurable Recommender Systems Simulation Platform')
+
+with open('README.md', 'r') as fh:
+  long_description = fh.read()
+
+setup(
+    name='recsim',
+    version='0.1.4',
+    author='The RecSim Team',
+    author_email='no-reply@google.com',
+    description=recsim_description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/google-research/recsim',
+    packages=find_packages(exclude=['docs']),
+    classifiers=[  # Optional
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: Apache Software License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+    ],
+    install_requires=install_requires,
+    project_urls={  # Optional
+        'Documentation': 'https://github.com/google-research/recsim',
+        'Bug Reports': 'https://github.com/google-research/recsim/issues',
+        'Source': 'https://github.com/google-research/recsim',
+    },
+    license='Apache 2.0',
+    keywords='recsim reinforcement-learning recommender-system simulation'
+)
