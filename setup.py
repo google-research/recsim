cc5e7db6cf381db22486f63d9506b9a64d401af3
RecSim Team <no-reply@google.com>
2019-09-27 17:37:37 -0700

    Project import generated by Copybara.
    PiperOrigin-RevId: 271681368

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='recsim',
-    version='0.1.4',
+    version='0.1.5',
     author='The RecSim Team',
     author_email='no-reply@google.com',
     description=recsim_description,
